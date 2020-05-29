ce2ec46bbc3a1e2a55acd058525236f1e60c5a8e
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2020-05-29 13:15:32 +0530

    Create If.py

---
+++ If.py
@@ -0,0 +1,9 @@
+# This program prints whether a person is eligible to vote or not
+
+age = int(input('Enter the age of person : '))
+
+if(age >= 18):
+  print('Yes, the person is eligible to vote')
+  
+else:
+  print('No, the person is not eligible to vote'))
