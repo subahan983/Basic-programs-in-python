822a86c8cb08debba400c494d59b0a468fdb1efe
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2020-05-29 13:12:54 +0530

    Create Swap.py

---
+++ Swap.py
@@ -0,0 +1,11 @@
+# In this program, we are going to swap two numbers 
+
+a = int(input('Enter first number : '))
+
+b = int(input('Enter second number : '))
+
+print('Numbers before swapping : a = ',a,' b = ',b)
+
+a,b = b,a
+
+print('Numbers after swapping : a = ',a,' b = ',b)
