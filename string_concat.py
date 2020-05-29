d2bce29e97eb56906fe9bdf36a9c866c35299138
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2020-05-29 13:02:50 +0530

    Create string_concat.py

---
+++ string_concat.py
@@ -0,0 +1,7 @@
+# In this program, A new string is formed by concatenaing two strings 
+
+string1 = input('Enter your first name : ')
+
+string2 = input('Enter your last name : ')
+
+print('Your name is ' + string1 + ' ' + string2)
