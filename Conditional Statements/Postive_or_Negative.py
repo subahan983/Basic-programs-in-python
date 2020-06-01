97f158d5e9c07981f3b74e79d0f536ecb9659f4a
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2020-06-01 14:22:25 +0530

    Create Postive_or_Negative.py

---
+++ Conditional Statements/Postive_or_Negative.py
@@ -0,0 +1,16 @@
+# This program prints whether the number is positive or negative
+
+a = 23
+
+b = -23
+
+if(a >= 0):
+  print(a,'is positive')
+else:
+  print(a,'is negative')
+  
+if(b >= 0):
+  print(b,'is positive')
+else:
+  print(b,'is negative')
+  
