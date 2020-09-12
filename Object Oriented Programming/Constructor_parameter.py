54746a4358e68516f4fee728cb10c7aea9c7b494
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2020-09-12 13:53:09 +0530

    Update Constructor_parameter.py

--- Object Oriented Programming/Constructor_parameter.py
+++ Object Oriented Programming/Constructor_parameter.py
@@ -1,1 +1,13 @@
+# This program shows how to pass arguments to a parameterized constructor.
 
+class Another:
+  def __init__(self, a, b):  # Parameterized constructor. 
+    self.a = a 
+    self.b = b
+    
+  def add(self):
+    print('The addition of {} and {} is {}'.format(a, b, a+b))
+    
+    
+obj = Another(10,20) # Passing 10 and 20 to the Another class constructor.
+obj.add()            # Calling the add method in Another class.
