cbf64d26fac6b3071b3add7cc1fec54efd361f6a
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2020-09-12 13:48:58 +0530

    Update Constructor.py

--- Object Oriented Programming/Constructor.py
+++ Object Oriented Programming/Constructor.py
@@ -2,4 +2,12 @@
 
 class MyClass:
   def __init__(self):
+    print('I am in constructor')
+   
+  def hello(self):
+    print('I am in hello method')
+    
+obj = MyClass() # Creating an object reference to MyClass class which invokes the default constructor.
+obj.hello() # Calling the hello method in the MyClass class.
+
     
