a5e735a63d8c5519de81b630469142f8eca8de95
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2020-09-12 13:43:52 +0530

    Update creating_class.py

--- Object Oriented Programming/creating_class.py
+++ Object Oriented Programming/creating_class.py
@@ -1,2 +1,12 @@
+# This program is just an introduction to Object Oriented Programming in python.
+# This program includes creation of a class and an object reference to it.
+
 class First:
+  def hello(self):
+    print('Hello')
+
+    
+obj = First() # Creating object reference to the First class.
+obj.hello()   # hello is the method in the First class accessed by the obj object reference.
+
   
