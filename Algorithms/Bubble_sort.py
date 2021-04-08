1088c38b850624131d0412a0f961e990b27f4cc0
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2021-04-08 20:45:19 +0530

    Create Bubble_sort.py

---
+++ Algorithms/Bubble_sort.py
@@ -0,0 +1,16 @@
+li = [80, 20, 100, 102, 14, 34]
+
+print('List before sorting is : ')
+
+for i in li:
+        print(i, end = ' ')
+print()
+
+for i in range(len(li)):
+        for j in range(len(li) - i - 1):
+                if(li[j] > li[j + 1]):
+                        li[j], li[j+1] = li[j+1], li[j]
+print('List after sorting is : ')
+
+for i in li:
+        print(i, end = ' ')
