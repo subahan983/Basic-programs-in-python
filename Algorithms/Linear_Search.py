98739759c26daccde57f8fd1ffdac0484f0749a6
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2021-04-08 20:41:56 +0530

    Update Linear_Search.py

--- Algorithms/Linear_Search.py
+++ Algorithms/Linear_Search.py
@@ -1,1 +1,8 @@
-n = int(input("Enter the size of array : "))
+li = [1, 2, 10, 12, 14, 34]
+key = 34
+for i in range(len(li)):
+  if(li[i] == key):
+    print(key, 'is present at : ', i, 'index')
+    break
+else:
+  print(key, 'is not in the list')
