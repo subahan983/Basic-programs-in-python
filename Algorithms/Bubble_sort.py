2c3512869216a7fcd5d80fde4fddf08da0580a11
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2021-04-08 20:52:58 +0530

    Update Bubble_sort.py

--- Algorithms/Bubble_sort.py
+++ Algorithms/Bubble_sort.py
@@ -1,3 +1,5 @@
+                                        # BUBBLE SORT
+
 li = [80, 20, 100, 102, 14, 34]
 
 print('List before sorting is : ')
@@ -9,7 +11,7 @@ print()
 for i in range(len(li)):
         for j in range(len(li) - i - 1):
                 if(li[j] > li[j + 1]):
-                        li[j], li[j+1] = li[j+1], li[j]
+                        li[j], li[j+1] = li[j+1], li[j] # swaps two numbers if left one is greater than the right one.
 print('List after sorting is : ')
 
 for i in li:
