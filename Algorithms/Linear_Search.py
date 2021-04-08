e870072b591519a78b329f1ae1fb2c8761157268
Subahan Ali Khan Patan <62606941+subahan983@users.noreply.github.com>
2021-04-08 20:54:13 +0530

    Update Linear_Search.py

--- Algorithms/Linear_Search.py
+++ Algorithms/Linear_Search.py
@@ -1,8 +1,8 @@
 li = [1, 2, 10, 12, 14, 34]
 key = 34
 for i in range(len(li)):
-  if(li[i] == key):
+  if(li[i] == key): # Compares with every element in the list with the given key element.
     print(key, 'is present at : ', i, 'index')
     break
-else:
+else: # Enters the else block if the key is not present in the list.
   print(key, 'is not in the list')
